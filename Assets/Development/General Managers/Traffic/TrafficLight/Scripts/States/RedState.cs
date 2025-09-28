8518ef62b6f0a1754ce1641533f14f0a76f4081f
JAHHH204 <barona.dev@gmail.com>
2025-09-28 15:18:53 -0500

    Traffic Lights, Cars,LightChanger,Manager
    -Traffic lights use state machine to set color and state
    -Light changer has the functions to set the state and light color
    -Traffic manager finds and groups lights into 2 groups
    -Cars are just basic nav agents for now
    *WIP but merging to have backup point if needed

--- Assets/Development/General Managers/Traffic/TrafficLight/Scripts/States/RedState.cs
+++ Assets/Development/General Managers/Traffic/TrafficLight/Scripts/States/RedState.cs
@@ -6,22 +6,26 @@ public class RedState : ITrafficInterface
     [SerializeField] private TrafficLightChanger lightChanger;
     [SerializeField] private ETrafficLightState light;
     [SerializeField] private float timer;
-    [SerializeField] private float duration;
+    [SerializeField] private float duration = 10f;
 
     public RedState(TrafficLightChanger lightChanger) { this.lightChanger = lightChanger; }
 
     public void EnterTrafficState()
     {
         timer = 0f;
+
     }
 
     public void UpdateTrafficState() 
-    { 
-        timer+= Time.deltaTime;
+    {
+
+        timer += Time.deltaTime;
         if (timer >= duration)
         {
-            lightChanger.ChangeLightState(new GreenState(lightChanger),ETrafficLightState.Red);
+            lightChanger.ChangeLightState(new GreenState(lightChanger),ETrafficLightState.Green);
+
         }
+
     }
 
     public void ExitTrafficState()
