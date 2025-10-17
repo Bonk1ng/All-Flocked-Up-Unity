6de77011cfa187413463749768d2521d6b91f26b
JAHHH204 <barona.dev@gmail.com>
2025-10-17 00:20:37 -0500

    SaveSlot,SaveWindow,SaveLoadHandler
    -Updates to player prefab
    -Added PlayerSaveLoadHandler
    -Updates to UI_SaveSLot and prefab
    -Updates to UI_SaveWindow and prefab
    -Updates to pause menu
    -Save window will fill with found save files from folder or empty slot if none
    -Functionality almost complete but will need QOL updates for screenshot, possible bugs, text/canvas updates
    -WIP

--- Assets/Development/General Managers/SaveLoad/SaveLoad/UI_SaveSlot.cs
+++ Assets/Development/General Managers/SaveLoad/SaveLoad/UI_SaveSlot.cs
@@ -1,16 +1,72 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_SaveSlot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private SaveSlotInfo data;
+    public SaveData saveData;
+    private SaveSlotManager slotManager;
+    public Image saveImage;
+    public TextMeshProUGUI playerNameText;
+    public TextMeshProUGUI lastSaveText;
+    public Button saveLoadButton;
+
+    private SaveSlotInfo slotInfo;
+    private UI_SaveWindow saveWindow;
+    public bool isSaving;
+
+    private void CheckSaveLoad()
     {
-        
+        if (!isSaving)
+        {
+            CallLoad();
+        }else { CallSave(); }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init(SaveSlotInfo info, SaveSlotManager manager, UI_SaveWindow window, bool saving = true)
     {
-        
+        slotInfo = info;
+        slotManager = manager;
+        saveWindow = window;
+        isSaving = saving;
+
+        UpdateSlotUI();
+
+        saveLoadButton.onClick.AddListener(() =>
+        {
+            if (isSaving && slotInfo.exists)
+            {
+                // Confirm overwrite
+                saveWindow.OpenConfirmWindow(this);
+            }
+            else
+            {
+                CheckSaveLoad();
+            }
+        });
     }
+    private void GetSlot()
+    {
+
+    }
+
+    private void CallSave()
+    {
+        SaveData data = FindFirstObjectByType<PlayerSaveLoadHandler>().saveData;
+        SaveSlotManager.SaveToSlot(slotInfo.slotIndex, data, true);
+        UpdateSlotUI();
+    }
+
+    private void CallLoad()
+    {
+        SaveData loadedData = SaveSlotManager.LoadFromSlot(slotInfo.slotIndex, true);
+    }
+
+    private void UpdateSlotUI()
+    {
+        playerNameText.text = slotInfo.exists ? slotInfo.playerName : "Empty Slot";
+        lastSaveText.text = slotInfo.exists ? slotInfo.lastSaved.ToString() : "--/--/--";
+    }
+
 }
