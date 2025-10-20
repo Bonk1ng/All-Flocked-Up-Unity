fea19715bef8cf5fe4c5ec8fa1cbcef49c10efe2
JAHHH204 <barona.dev@gmail.com>
2025-10-20 00:21:29 -0500

    Steamworks.Net Integration
    -Imported and Set up Steamworks.Net API
    -This will have all the functions required to use steam functionality... friends, achievements,etc and will be needed to upload the build to steam
    -Added SteamInit script and prefab that will init steam and keep it loaded with a singleton pattern
    -Core implemented but will need each system to call this to have it function
    *Pushing before merge gets too large*

---
+++ Assets/Development/General Managers/Game/SteamInit.cs
@@ -0,0 +1,70 @@
+using Steamworks;
+using UnityEngine;
+
+public class SteamInit : MonoBehaviour
+{
+    private static SteamManager instance;
+    private static bool isInitialized = false;
+
+    public static bool Initialized => isInitialized;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        try
+        {
+            if (SteamAPI.RestartAppIfNecessary((AppId_t)480))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+        catch (System.DllNotFoundException e)
+        {
+            Debug.LogError("[Steamworks.NET] Could not find steam_api.dll/so/dylib: " + e);
+            Application.Quit();
+            return;
+        }
+
+        isInitialized = SteamAPI.Init();
+        if (!isInitialized)
+        {
+            Debug.LogError("[Steamworks.NET] SteamAPI_Init() failed. Is Steam running?");
+            Application.Quit();
+        }
+        else
+        {
+            Debug.Log("[Steamworks.NET] Initialized successfully!");
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (isInitialized)
+            SteamAPI.RunCallbacks();
+    }
+
+    private void Update()
+    {
+        if (isInitialized)
+            SteamAPI.RunCallbacks();
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (isInitialized)
+        {
+            SteamAPI.Shutdown();
+            isInitialized = false;
+            Debug.Log("[Steamworks.NET] Shutdown complete.");
+        }
+    }
+}
