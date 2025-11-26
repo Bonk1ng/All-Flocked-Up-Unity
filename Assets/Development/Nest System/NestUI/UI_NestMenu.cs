a252cb4538706ff31872e385d52bf17c8f8f350e
JAHHH204 <barona.dev@gmail.com>
2025-11-25 23:40:00 -0600

    Cropped UI Assets, Canvas Updates, Nest Updates
    -Cropped and Imported newest UI Assets
    -Updates to some Canvas Prefabs to add new Art
    -Updates to NestUI and script

--- Assets/Development/Nest System/NestUI/UI_NestMenu.cs
+++ Assets/Development/Nest System/NestUI/UI_NestMenu.cs
@@ -1,18 +1,41 @@
+using NUnit.Framework;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class UI_NestMenu : MonoBehaviour
 {
+    [Header("Icons")]
     [SerializeField] private GameObject dayIcon;
     [SerializeField] private GameObject nightIcon;
-    [SerializeField] private Button closeButton;
+    [Header("Buttons")]
+    [SerializeField] private Button closeMainButton;
     [SerializeField] private Button invButton;
     [SerializeField] private Button sleepButton;
     [SerializeField] private Button closeInvButton;
-    public UI_CanvasController canvasController;
+    [SerializeField] private Button statsButton;
+    [SerializeField] private Button closeStatsButton;
+    [SerializeField] private Button scrapbookButton;
+    [SerializeField] private Button sbNextPage;
+    [SerializeField] private Button sbPrevPage;
+    [SerializeField] private Button closeSbButton;
+    [Header("Stats")]
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI staminaText;
+    [SerializeField] private TextMeshProUGUI poopText;
+    [Header("ScrapBookRefs")]
+    [SerializeField] private List<Image> photoList;
+    [SerializeField] private List<Sprite> photos;
+    [Header("Panels")]
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject invPanel;
+    [SerializeField] private GameObject statsPanel;
+    [SerializeField] private GameObject scrapbookPanel;
+    
+    [Header("Components")]
     [SerializeField] private S_DayNightCycle dayNightSystem;
+    public UI_CanvasController canvasController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
@@ -23,10 +46,17 @@ public class UI_NestMenu : MonoBehaviour
     }
     void Start()
     {
-        closeButton.onClick.AddListener(CloseNestMenu);
+        closeMainButton.onClick.AddListener(CloseNestMenu);
         invButton.onClick.AddListener(OpenNestInventory);
         sleepButton.onClick.AddListener(SleepAtNest);
         closeInvButton.onClick.AddListener(CloseNestInventory);
+        statsButton.onClick.AddListener(OpenStats);
+        closeStatsButton.onClick.AddListener(CloseStats);
+        scrapbookButton.onClick.AddListener(OpenScrapbook);
+        closeSbButton.onClick.AddListener(CloseScrapbook);
+        sbNextPage.onClick.AddListener(SBNextPage);
+        sbPrevPage.onClick.AddListener(SBPrevPage);
+
     }
 
     private void Update()
@@ -72,4 +102,38 @@ public class UI_NestMenu : MonoBehaviour
         dayNightSystem.timeOfDay += 0.5f;
     }
 
+
+    private void OpenStats()
+    {
+        mainPanel.SetActive(false);
+        statsPanel.SetActive(true);
+    }
+
+    private void CloseStats()
+    {
+        mainPanel?.SetActive(true);
+        statsPanel.SetActive(false);
+    }
+
+    private void OpenScrapbook()
+    {
+        mainPanel.SetActive(false);
+        scrapbookPanel.SetActive(true);
+    }
+
+    private void CloseScrapbook()
+    {
+        mainPanel?.SetActive(true);
+        scrapbookPanel.SetActive(false);
+    }
+
+    private void SBNextPage()
+    {
+
+    }
+
+    private void SBPrevPage()
+    {
+
+    }
 }
